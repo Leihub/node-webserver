4dbd33ee54e86a744f8a4ac263f50aebf7898fb7
leiyuan <814287066@qq.com>
2017-10-09 18:39:05 +0800

    add data markdown

--- node-vue/src/components/aside.vue
+++ node-vue/src/components/aside.vue
@@ -1,8 +1,19 @@
 <template>
-  
+  <div class="aside">
+      <img src="../assets/logo.png" alt="userlogo" class="userlogo">
+      <p class="username"></p>
+      <ul>
+          <li v-for=""></li>
+      </ul>
+  </div>
 </template>
 <script>
 export default {
+    data(){
+        return {
+            
+        }
+    }
   
 }
 </script>
